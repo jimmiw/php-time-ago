a9c168795895a3a2332bcd825d9b92d04ab45158
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2015-05-26 10:19:25 +0200

    added the english translations

---
+++ translations/en.php
@@ -0,0 +1,15 @@
+<?php
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "1 day",
+  'aboutOneHour' => "about 1 hour",
+  'aboutOneMonth' => "about 1 month",
+  'aboutOneYear' => "about 1 year",
+  'days' => "%s days",
+  'hours' => "%s hours",
+  'lessThanAMinute' => "less than a minute",
+  'lessThanOneHour' => "%s minutes",
+  'months' => "%s months",
+  'oneMinute' => "1 minute",
+  'years' => "over %s years"
+);
