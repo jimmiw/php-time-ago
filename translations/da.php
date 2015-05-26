4a84f6254d6b4e2ab64dac3f59071bc2258229e4
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2015-05-26 10:55:21 +0200

    added danish translations.

---
+++ translations/da.php
@@ -0,0 +1,15 @@
+<?php
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "1 dag",
+  'aboutOneHour' => "cirka 1 time",
+  'aboutOneMonth' => "cirka 1 måned",
+  'aboutOneYear' => "cirka 1 år",
+  'days' => "%s dage",
+  'hours' => "%s timer",
+  'lessThanAMinute' => "mindre end et minut",
+  'lessThanOneHour' => "%s minutter",
+  'months' => "%s måneder",
+  'oneMinute' => "1 minut",
+  'years' => "over %s år"
+);
