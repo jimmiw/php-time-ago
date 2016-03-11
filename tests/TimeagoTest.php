b4a7c70e6fa861b4f46786e2017722fbcaa7f50b
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2016-03-11 15:25:07 +0100

    Test environment is up and running! Added phpunit to composer.json in require-dev. Added a phpunit.xml file. Added an "isAlive" test for the timeago module.

---
+++ tests/TimeagoTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Testing timeago dates
+ * @author jimmiw
+ * @since 2016-03-11
+ */
+class TimeagoTest extends PHPUnit_Framework_TestCase
+{
+    public function testIsAlive()
+    {
+        $this->assertTrue(true);
+    }
+}
