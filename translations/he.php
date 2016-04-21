dfeaef7cae87069f6276c56affd5cc2efe1abaa9
Radik <radik.chernyshov@gmail.com>
2016-04-21 16:30:52 +0300

    add he translation

---
+++ translations/he.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'aboutOneDay' => "אתמול",
+    'aboutOneHour' => "לפני שעה",
+    'aboutOneMonth' => "לפני חודש",
+    'aboutOneYear' => "שנה שעברה",
+    'days' => "לפני %s ימים",
+    'hours' => "לפני %s שעות",
+    'lessThanAMinute' => "פחות מדקה",
+    'lessThanOneHour' => "לפני %s דקות",
+    'months' => "לפני %s חודשים",
+    'oneMinute' => "לפני דקה",
+    'years' => "יותר מ-%s שנים"
+];
