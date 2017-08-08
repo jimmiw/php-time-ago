826a5a5d00bd64dad8cfdd9d2f819b7c6c09efa2
Lê Thành <baivong@users.noreply.github.com>
2017-08-08 09:43:29 +0700

    Create vi.php

---
+++ translations/vi.php
@@ -0,0 +1,16 @@
+<?php
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "1 ngày trước",
+  'aboutOneHour' => "1 giờ trước",
+  'aboutOneMonth' => "1 tháng trước",
+  'aboutOneYear' => "1 năm trước",
+  'days' => "%s ngày trước",
+  'hours' => "%s giờ trước",
+  'lessThanAMinute' => "gần 1 phút trước",
+  'lessThanOneHour' => "%s phút trước",
+  'months' => "%s tháng trước",
+  'oneMinute' => "1 phút trước",
+  'years' => "hơn %s năm trước",
+  'never' => 'Không xác định'
+);
