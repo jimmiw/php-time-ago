81e7e17b098b8eeb27d639b3ebc91f189794f50b
Niko Peikrishvili <nikopeikrishvili@Nikos-MacBook-Air.local>
2017-08-11 09:40:26 +0400

    Added Georgian translation

---
+++ translations/ge.php
@@ -0,0 +1,16 @@
+<?php
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "გუშინ",
+  'aboutOneHour' => "1 საათის წინ",
+  'aboutOneMonth' => "1 თვის წინ",
+  'aboutOneYear' => "1 წლის წინ",
+  'days' => "%s დღის წინ",
+  'hours' => "%s საათის წინ",
+  'lessThanAMinute' => "წუთზე ნაკლები",
+  'lessThanOneHour' => "%s წუთის წინ",
+  'months' => "%s თვის წინ",
+  'oneMinute' => "1 წუთის წინ",
+  'years' => "%s წელზე მეტი",
+  'never' => 'არასდროს'
+);
