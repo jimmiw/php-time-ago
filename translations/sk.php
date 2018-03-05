8f54fcdaf64de69434d856dbe0cac46c0bd333bc
Frantisek Vodslon <frantisek.vodslon@gmail.com>
2018-03-05 12:33:23 +0100

    SK translation added

---
+++ translations/sk.php
@@ -0,0 +1,15 @@
+<?php
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "pred 1 dňom",
+  'aboutOneHour' => "pred 1 hodinou",
+  'aboutOneMonth' => "před 1 měsícem",
+  'aboutOneYear' => "pred 1 rokom",
+  'days' => "pred %s dňami",
+  'hours' => "pred %s hodinami",
+  'lessThanAMinute' => "pred menej ako minútou",
+  'lessThanOneHour' => "pred  %s minútami",
+  'months' => "pred %s mesiacmi",
+  'oneMinute' => "pred 1 minútou",
+  'years' => "pred %s rokmi"
+);
