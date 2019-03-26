6218016ac564863493e4f0fe63c178fa364563d6
Dhuta Pratama <me@dhutapratama.com>
2019-03-27 02:27:44 +0700

    Translation to Indonesian Language (#1)
    Hello, this is my contribution for indonesian language.
    Please attach this commit to your project.

---
+++ src/Westsworld/TimeAgo/Translations/Id.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Westsworld\TimeAgo\Translations;
+
+use \Westsworld\TimeAgo\Language;
+
+/**
+ * Indonesian translations
+ */
+class En extends Language
+{
+    public function __construct()
+    {
+        $this->setTranslations([
+            'aboutOneDay' => "1 hari",
+            'aboutOneHour' => "1 jam",
+            'aboutOneMonth' => "1 bulan",
+            'aboutOneYear' => "1 tahun",
+            'days' => "%s hari",
+            'hours' => "%s bulan",
+            'lessThanAMinute' => "baru saja",
+            'lessThanOneHour' => "%s menit",
+            'months' => "%s bulan",
+            'oneMinute' => "1 menit",
+            'years' => "%s tahun",
+            'never' => 'Tidak pernah'
+        ]);
+    }
+}
