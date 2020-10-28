f83f2dd398c4d1c2d0f05a417c3248d462280e02
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2020-10-28 21:02:38 +0100

    Adding shorter English class
    I've added a shorter English language class following my issue: 
    https://github.com/jimmiw/php-time-ago/issues/91

---
+++ src/Westsworld/TimeAgo/Translations/En_short.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Westsworld\TimeAgo\Translations;
+
+use \Westsworld\TimeAgo\Language;
+
+/**
+ * Short English translations
+ */
+class En_short extends Language
+{
+    public function __construct()
+    {
+        $this->setTranslations([
+            'aboutOneDay' => "1d",
+            'aboutOneHour' => "~1h",
+            'aboutOneMonth' => "~1M",
+            'aboutOneYear' => "~y",
+            'days' => "%sd",
+            'hours' => "%sh",
+            'lessThanAMinute' => "<1m",
+            'lessThanOneHour' => "%sm",
+            'months' => "%sM",
+            'oneMinute' => "1m",
+            'years' => ">%sy"
+        ]);
+    }
+}
